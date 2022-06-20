9c5bffbf725c9241ea777bd9fc5910454b3d075f
HaleemaGit <mustaphahaleema3@gmail.com>
2022-06-20 23:56:20 +0100

    Details created successfully

---
+++ main.js
@@ -0,0 +1,5 @@
+document.write("Name: Haleema Mustapha");
+document.write("<br>");    
+document.write("Height: 5 foot 4 inches");  
+document.write("<br>");    
+document.write("Country: Nigeria")  
